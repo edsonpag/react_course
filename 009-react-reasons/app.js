2e1aa25ac0f969a443a471dc668c51a665928152
edsonpag <edsonsilvapagani@gmail.com>
2022-05-13 05:50:56 -0300

    react reasons update

--- 009-react-reasons/app.js
+++ 009-react-reasons/app.js
@@ -1,21 +1,46 @@
-function Page() {
-    return(
-        <div>
-            <header>
-                <nav>
-                    <img src="../react-logo.svg" style={{ width: "4rem" }}/>
-                    <h1>Razões do porque eu estou aprendendo React!</h1>
-                </nav>
-            </header>
-            <h2>Por que aprender React?</h2>
-            <ol>
+function Header() {
+    return (
+        <header>
+            <nav className="navbar">
+                <img src="../react-logo.svg" className="logo"/>
+                <ul className="nav-items">
+                    <li>Preço</li>
+                    <li>Sobre</li>
+                    <li>Contato</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+function Main() {
+    return (
+        <main>
+            <h1>Razões do porque eu estou aprendendo React!</h1>
+            <ul className="main-items">
                 <li>Melhorar minhas skills</li>
                 <li>React é maravilhoso</li>
                 <li>Conseguir um emprego como desenvolvedor</li>
-            </ol>
-            <footer>
-                <p>&copy; 2001 Pagani. Todos os direitos reservados.</p>
-            </footer>
+            </ul>
+        </main>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; 2001 Pagani. Todos os direitos reservados.</p>
+        </footer>
+    )
+}
+
+
+function Page() {
+    return(
+        <div>
+            <Header />
+            <Main />
+            <Footer />
         </div>
         
     )
