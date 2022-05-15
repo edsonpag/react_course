f52ae0cd86b4045b0886870b63ab64564bcbc87c
edsonpag <edsonsilvapagani@gmail.com>
2022-05-15 07:17:05 -0300

    add card container

--- 012-airbnb-clone/src/components/Card/Card.js
+++ 012-airbnb-clone/src/components/Card/Card.js
@@ -7,29 +7,30 @@ import cardStar from "../../images/star.png";
 
 function Card() {
     return (
-        <section className="card">
-            <div className="card-image-container">
-                <p className="card-status">sold out</p>
-                <img src={cardImage} className="card-image"/>
+        <section className="card-container">
+            <div className="card">
+                <div className="card-image-container">
+                    <p className="card-status">sold out</p>
+                    <img src={cardImage} className="card-image"/>
+                </div>
+
+                <div className="card-description">
+                    <div className="line-1">
+                        <img src={cardStar} className="card-star"/>
+                        <p className="card-grade">5.0</p>
+                        <p className="card-reviews">(6)</p>
+                        <p className="card-country">USA</p>
+                    </div>
+
+                    <div className="line-2">
+                        <p className="card-text">Life lessons with Katie Zaferes</p>
+                    </div>
+
+                    <div className="line-3">
+                        <p className="card-price"><b>From $138</b> / person</p>
+                    </div>
+                </div> 
             </div>
-
-            <div className="card-description">
-                <div className="line-1">
-                    <img src={cardStar} className="card-star"/>
-                    <p className="card-grade">5.0</p>
-                    <p className="card-reviews">(6)</p>
-                    <p className="card-country">USA</p>
-                </div>
-
-                <div className="line-2">
-                    <p className="card-text">Life lessons with Katie Zaferes</p>
-                </div>
-
-                <div className="line-3">
-                    <p className="card-price"><b>From $138</b> / person</p>
-                </div>
-            </div>
-            
         </section>
     );
 }
