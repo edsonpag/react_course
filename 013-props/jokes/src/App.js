eab874741d4f3fceb4605e28bb148c6709ad1d87
edsonpag <edsonsilvapagani@gmail.com>
2022-05-18 09:34:58 -0300

    update joke app

--- 013-props/jokes/src/App.js
+++ 013-props/jokes/src/App.js
@@ -3,44 +3,17 @@ import React from "react";
 import "./App.css";
 
 import Joke from "./components/Joke/Joke";
+import jokesData from "./components/Joke/JokesData.json";
+
 
 function App() {
+    const jokesElements = jokesData.map((joke) => {
+        return <Joke setup={joke.setup} punchline={joke.punchline}/>
+    });
+
     return (
         <div className="jokes-container">
-            <Joke 
-            setup="Whats the best thing about Switzerland?"
-            punchline="I dont know, but the flag is a big plus."
-            upvotes = {10}
-            downvotes = {3}
-            isPun = {true}
-            comments = {[
-                {
-                    id: 1,
-                    name: "john",
-                    body: "This is soo fun",
-                    title: "Nice. I love"
-                }
-            ]}
-            />
-
-            <Joke 
-            setup="I invented a new word!"
-            punchline="Plagiarism!"
-            />
-
-            <Joke 
-            setup="Did you hear about the mathematician who's afraid of negative numbers?"
-            punchline="Because every play has a cast. Here are some dark jokes to check out if you have a morbid sense of humor."
-            />
-
-            <Joke 
-            punchline="There's no menu: You get what you deserve."
-            />
-
-            <Joke 
-            punchline="He was just going through a stage."
-            />
-            
+            {jokesElements}
         </div>
         
     );
