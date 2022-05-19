2caf4d4b4358cf9e49ce81a79d7dbdd9da67116b
edsonpag <edsonsilvapagani@gmail.com>
2022-05-19 19:51:51 -0300

    refeito o boxes challange para treinar

--- 017-useState/src/components/Boxes/Box.js
+++ 017-useState/src/components/Boxes/Box.js
@@ -4,12 +4,11 @@ function Box(props) {
 
     const styles = {
         backgroundColor: props.on ? "#222222" : "transparent"
-    }
+    };
 
-    return(
-        <div style={styles} className="box" onClick={() => props.toggle(props.id)}></div>
+    return (
+        <div className="box" onClick={props.handleClick} style={styles}></div>
     );
 }
-
 
 export default Box;
