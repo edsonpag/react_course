c0353353971db1e7dc223ccc125603045dfd644b
edsonpag <edsonsilvapagani@gmail.com>
2022-05-23 17:18:23 -0300

    ad new tweet btn and navbar

--- 024-twitter-clone/src/App.js
+++ 024-twitter-clone/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 
 import Header from './components/Header/Header';
 import Tweet from './components/Tweet/Tweet';
+import NewTweetButton from './components/NewTweetButton/NewTweetButton';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
       <Tweet />
       <Tweet />
       <Tweet />
+      <NewTweetButton />
+      <Navbar />
     </div>
   );
 }
