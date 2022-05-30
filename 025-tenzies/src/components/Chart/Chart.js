227163c30ce4ac02428786a96550e175dde651b6
edsonpag <edsonsilvapagani@gmail.com>
2022-05-30 16:02:40 -0300

    add a simple animation

--- 025-tenzies/src/components/Chart/Chart.js
+++ 025-tenzies/src/components/Chart/Chart.js
@@ -6,17 +6,16 @@ function Chart(props) {
 
     const [ isShow, setIsShow ] = useState(false);
 
-    function showChart() {
+    function toggleChart() {
         setIsShow(prevIsShow => !prevIsShow);
     }
-
 
     return (
         <>
             {isShow 
             ?
                 <div className="chart-container">
-                    <div className="chart-icon close-animation" onClick={showChart}>
+                    <div className="close-icon" onClick={toggleChart}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
                             <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
                         </svg>
@@ -37,7 +36,7 @@ function Chart(props) {
                     </div>
                 </div>
             : 
-            <div className="chart-icon" onClick={showChart}>
+            <div className="chart-icon" onClick={toggleChart}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bar-chart" viewBox="0 0 16 16" >
                     <path d="M4 11H2v3h2v-3zm5-4H7v7h2V7zm5-5v12h-2V2h2zm-2-1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1h-2zM6 7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7zm-5 4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-3z"
                     />
