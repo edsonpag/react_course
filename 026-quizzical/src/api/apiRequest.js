8751d106c4f93f631090db8a139b54ba92105db2
edsonpag <edsonsilvapagani@gmail.com>
2022-06-02 07:42:58 -0300

    ajustado bug de encode

--- 026-quizzical/src/api/apiRequest.js
+++ 026-quizzical/src/api/apiRequest.js
@@ -2,10 +2,24 @@ import axios from "axios";
 
 const apiRequest = async () => {
     try {
-        const apiUrl = "https://opentdb.com/api.php?amount=5&type=multiple";
+        const apiUrl = "https://opentdb.com/api.php?amount=5&type=multiple&encode=base64";
         const response = await axios.get(apiUrl);
+        const results = response.data.results    
 
-        return response.data.results;
+        for(let obj of results) {
+            for(let propriety in obj) {
+                if(Array.isArray(obj[propriety])) {
+                    obj[propriety] = obj[propriety].map((item) => {
+                        return atob(item);
+                    });
+                } else {
+                    obj[propriety] = atob(obj[propriety]);
+                }
+            }
+        }
+
+
+        return results;
 
     } catch (error) {
         console.log(error);
