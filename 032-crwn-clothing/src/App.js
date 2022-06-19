a272d31b9c156af7eb73bbc7beda362163895adc
edsonpag <edsonsilvapagani@gmail.com>
2022-06-19 20:36:05 -0300

    Criar uma div com um style de backgroundImage Definir a image 100% do height e width do container Definir que o container não tenha overflow Adicionar efeito de zoom

--- 032-crwn-clothing/src/App.js
+++ 032-crwn-clothing/src/App.js
@@ -1,6 +1,55 @@
+import React from "react";
+
+import "./categories.scss";
+
 function App() {
+
+  const categories = [
+    {
+      "id": 1,
+      "title": "hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+    },
+    {
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+    },
+    {
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+    },
+    {
+      "id": 4,
+      "title": "womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+    },
+    {
+      "id": 5,
+      "title": "mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
+  ];
+
+
   return (
-    <div>Hello World</div>
+    <div className="categories-container">
+      {categories.map((category) => {
+        return (
+          <div className="category-container" key={category.id}>
+            <div className="background-image" style={{
+              backgroundImage: `url(${category.imageUrl})`
+            }}/>
+            
+            <div className="category-body">
+              <h4>{category.title}</h4>
+              <p>Shop Now</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 }
 
